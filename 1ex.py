c59652ae8ef0a18038eb30d7cd08087807a776c5
mike <barbeque@users.noreply.github.com>
2018-11-30 22:24:15 -0700

    ** 1ex

--- 1ex.py
+++ 1ex.py
@@ -1,9 +1,17 @@
 frequency = 0
+import sys
+
+seen_freqs = {}
 
 with open('input1.txt') as i:
     lines = i.readlines()
 
-for line in lines:
-    frequency += int(line)
+while True:
+    for line in lines:
+        frequency += int(line)
+        if frequency in seen_freqs:
+            print "Seen %i before" % frequency
+            sys.exit(0)
+        seen_freqs[frequency] = True
 
 print "Freq = %i" % frequency
