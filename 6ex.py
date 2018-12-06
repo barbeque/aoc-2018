6f1bcd495e02bbea1fe94f83bef7f1f35ba98c45
mike <barbeque@users.noreply.github.com>
2018-12-06 12:55:53 -0700

    get_closest_to

--- 6ex.py
+++ 6ex.py
@@ -5,6 +5,19 @@ assert manhattan_distance(0, 0, 2, 1) == 3
 assert manhattan_distance(0, 0, 0, 0) == 0
 assert manhattan_distance(10, 5, 0, 0) == 15
 
+def closest_to(x, y, coords):
+    distances = []
+    for coord in coords:
+        distances.append((coord, manhattan_distance(x, y, coord[0], coord[1])))
+    least_to_most = sorted(distances, key=lambda kv: kv[1])
+
+    if least_to_most[0][1] == least_to_most[1][1]:
+        return None # equidistant
+    return least_to_most[0][0] # i want the coord, not the distance
+
+assert closest_to(0, 0, [(0, 1), (2, 3)]) == (0, 1)
+assert closest_to(0, 0, [(0, 1), (1, 0)]) == None
+
 with open('input6.txt') as i:
     lines = i.readlines()
 
